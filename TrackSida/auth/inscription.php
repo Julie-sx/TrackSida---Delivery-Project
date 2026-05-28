d6050b24a610eee1b03fe7a378a5679627a90fc4
TranLan07 <bob.anger10@gmail.com>
2026-05-28 06:08:46 +0200

    correct bug

--- TrackSida/auth/inscription.php
+++ TrackSida/auth/inscription.php
@@ -1,49 +1,50 @@
 <?php
 
 require_once('../script/bdd.php');
-
+echo('...');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo("reception data...");
     $prenom = isset($_POST['prenom']) ? safeInput($_POST['prenom']) : '';
     $nom = isset($_POST['nom']) ? safeInput($_POST['nom']) : '';
     $gender = isset($_POST['gender']) ? safeInput($_POST['gender']) : '';
     $email = isset($_POST['email']) ? safeInput($_POST['email']) : '';
-    $password = isset($_POST['password']) ? $_POST['password'] : '';
-    $password_confirm = isset($_POST['password_confirm']) ? $_POST['password_confirm'] : '';
+    $password = isset($_POST['password']) ? safeInput($_POST['password']) : '';
+    $password_confirm = isset($_POST['password_confirm']) ? safeInput($_POST['password_confirm']) : '';
     
     $cgu = isset($_POST['cgu']) ? 1 : 0;
-
+    echo("verif data set...");
     if (empty($prenom) || empty($nom) || empty($gender) || empty($email) || empty($password)) {
         header('Location: index.php?fail=err');
         exit;
     }
-
+    echo("f1...");
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         header('Location: index.php?fail=invalid_email');
         exit;
     }
-
+    echo("f2...");
     if (strlen($password) < 8) {
         header('Location: index.php?fail=pwd_too_short');
         exit;
     }
-
+    echo("f3...");
     if ($password !== $password_confirm) {
         header('Location: index.php?fail=pwd_mismatch');
         exit;
     }
-
+    echo("f4...");
     if ($cgu !== 1) {
         header('Location: index.php?fail=cgu_required');
         exit;
     }
-
+    echo("f5...");
     if ($_POST['password']!=$password){
         header('Location: index.php?fail=err');
         exit;
     }
-
+    echo("f6...");
     $password = hash('sha256',$password);
+    echo("password hash...");
     echo('try bdd insert');
     $bdd_findUSer = selectData("utilisateurs",["id_utilisateur"],["email"=>$email]);
 
