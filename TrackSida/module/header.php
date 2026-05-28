75dfcb3ab0ca6cfe15f31d20e01ba0f78aa02b20
Julie-sx <julie.sieux@efrei.net>
2026-05-28 16:54:15 +0200

    probleme de police

--- TrackSida/module/header.php
+++ TrackSida/module/header.php
@@ -2,6 +2,7 @@
 <head>
     <title>Header</title>
     <link rel="stylesheet" href="../css/style.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&display=swap" rel="stylesheet" />
 </head>
 <header>
     <a href="#" class="logo">Tracksida</a>
