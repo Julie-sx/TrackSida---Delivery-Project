aad8791a9f3c08a28926300753cc2957497e8c06
TranLan07 <bob.anger10@gmail.com>
2026-05-28 13:39:10 +0200

    try correctif

--- TrackSida/script/session.php
+++ TrackSida/script/session.php
@@ -10,4 +10,18 @@ if(!viewSession()){
     header('Location:/auth/');
 }
 
+function isAdmin(){
+    $userLevel=selectData('utilisateurs',['niveau'],['id_utilisateur'=>$_SESSION['user_id']]);
+    if(!empty($userLevel)){
+        $userLevel=intval($userLevel[0]['niveau']);
+        if($userLevel>=10){
+            return true;
+        }else{
+            return false;
+        }
+    }else{
+        return false;
+    }
+}
+
 ?>
