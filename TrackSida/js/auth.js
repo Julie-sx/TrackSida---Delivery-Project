fb0783d72a34d23e0692ea780524cf4b102b3f43
TranLan07 <bob.anger10@gmail.com>
2026-05-28 05:14:02 +0200

    ajout popup fail login

--- TrackSida/js/auth.js
+++ TrackSida/js/auth.js
@@ -28,3 +28,48 @@ function switchToLogin() {
 
 tabRegister.addEventListener('click', switchToRegister);
 tabLogin.addEventListener('click', switchToLogin);
+
+
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+
+const fail = urlParams.get('fail');
+
+if(fail){
+
+    const overlay = document.createElement('div');
+    overlay.className = 'popup-overlay';
+    overlay.id = 'dynamic-popup-overlay';
+
+    const popup = document.createElement('div');
+    popup.className = 'popup popup-err';
+
+    const h2 = document.createElement('h2');
+    h2.textContent = "Erreur -";
+
+    const p = document.createElement('p');
+    p.textContent = "Informations incorrects ou manquantes";
+
+    const closeBtn = document.createElement('button');
+    closeBtn.type = 'button';
+    closeBtn.className = 'btn-submit';
+    closeBtn.textContent = 'Fermer';
+
+    closeBtn.addEventListener('click', () => {
+        overlay.remove();
+    });
+
+    overlay.addEventListener('click', (e) => {
+        if (e.target === overlay) {
+            overlay.remove();
+        }
+    });
+
+    popup.appendChild(h2);
+    popup.appendChild(p);
+    popup.appendChild(closeBtn);
+    overlay.appendChild(popup);
+
+    document.body.appendChild(overlay);
+}
